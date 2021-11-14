2c24db45c9b46ca46bff8c6ab1d44d9c942b9dd8
spaff13 <spaff316@gmail.com>
2021-11-14 15:48:42 +0900

    이미지 업로드 및 파이썬 테스트

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var router = express.Router();
 const db = require('../my_modules/db');
+const multer = require('multer');
+const { PythonShell } = require("python-shell");
 
+const upload = multer({ dest: 'uploads/', limits: { fileSize: 5 * 1024 * 1024 } });
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
@@ -12,4 +15,20 @@ router.get('/dbtest', async (req, res)=>{
   return res.send(ret);
 });
 
+router.post('/imgtest', upload.single('file'/* img사용 가능?*/), (req, res)=>{
+  console.log(req.file);
+  res.send('ok');
+})
+
+router.get('/pytest',(req, res)=>{
+  let options = {
+    scriptPath: "my_modules",
+    args: ['value1', 'value2']
+  };
+  PythonShell.run("test.py", options, function(err, data) {
+    if (err) throw err;
+    console.log(data);
+    res.send('ok')
+  });
+})
 module.exports = router;
