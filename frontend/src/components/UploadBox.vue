4eac3857da456d9e82351a82d89a18abc89f81c2
dev2820 <second28200@gmail.com>
2021-11-16 15:17:30 +0900

    사진,영상 업로드에 사용할 uploadbox 생성

---
+++ frontend/src/components/UploadBox.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="upload-box">
+        <font-awesome-icon icon="upload"/>
+        <div>{{explain}}</div>
+    </div>
+</template>
+
+<script>
+//todo: 이미지 드래그해서 hover시 애니메이션, 
+export default {
+    props: {
+        explain:String
+    }
+}
+</script>
+
+<style>
+.upload-box {
+    border:1px dashed var(--placeholder-color);
+}
+</style>
