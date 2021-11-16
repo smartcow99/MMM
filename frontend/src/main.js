38fe937003d2431afd171a5eb9e5a34c3c44fa58
dev2820 <second28200@gmail.com>
2021-11-16 14:43:12 +0900

    icon 추가

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,11 +4,12 @@ import router from './router'
 import store from './store'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faUserSecret,faSearch,faTimes } from '@fortawesome/free-solid-svg-icons'
+import { faUserSecret,faSearch,faTimes,faUpload } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 library.add(faUserSecret)
 library.add(faSearch)
 library.add(faTimes)
+library.add(faUpload)
 
 const app = createApp(App);
 app.component('font-awesome-icon',FontAwesomeIcon);
