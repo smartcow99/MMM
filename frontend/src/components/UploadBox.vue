bc7e598ef67be0346b03253a7e63cd2a1615d541
dev2820 <second28200@gmail.com>
2021-11-16 21:33:37 +0900

    uploadbox 완성, upload 이벤트로 감지할 것

--- frontend/src/components/UploadBox.vue
+++ frontend/src/components/UploadBox.vue
@@ -1,7 +1,17 @@
 <template>
-    <div class="upload-box">
-        <font-awesome-icon icon="upload"/>
-        <div>{{explain}}</div>
+    <div id="dropzone"
+        :class="{'overing':overing}"
+        @dragenter="dragenterHandler($event)" 
+        @dragleave="dragleaveHandler($event)" 
+        @dragover.prevent
+        @drop.prevent="dropHandler($event)"
+        @click="clickHandler($event)"
+        class="dropzone upload-box">
+        <img v-if="!!src" :src="src"/>
+        <span v-else class="guide">
+            <font-awesome-icon icon="upload"/>
+            <div>{{explain}}</div>
+        </span>
     </div>
 </template>
 
@@ -10,6 +20,47 @@
 export default {
     props: {
         explain:String
+    },
+    data() {
+        return {
+            file: null,
+            overing: false,
+            reader: new FileReader(),
+            src:''
+        }
+    },
+    mounted() {
+        this.reader.addEventListener('load',()=>{
+            this.src = this.reader.result;
+        },false)  
+    },
+    methods: {
+        dragenterHandler(evt) {
+            this.overing = true;
+        },
+        dragleaveHandler(evt) {
+            this.overing = false;
+        },
+        dropHandler(evt) {
+            this.overing = false;
+            const files = evt.dataTransfer.files;
+            if(files.length>1) {
+                alert('한 장의 사진만 올려주세요.')
+            }
+            else {
+                const file = files[0];
+                if(!/^image\//.test(file.type)) {
+                    alert('이미지 파일만 가능합니다.')
+                }
+                else {
+                    this.reader.readAsDataURL(file);
+                    this.$emit('upload',file);
+                }
+            }
+        },
+        clickHandler(evt) {
+            alert('click')
+        }
     }
 }
 </script>
@@ -17,5 +68,10 @@ export default {
 <style>
 .upload-box {
     border:1px dashed var(--placeholder-color);
+}
+.overing {
+    background-color:var(--primary-color);
+    opacity:0.7;
+    color:var(--white-text-color);
 }
 </style>
