a96b132ef521540dd2b4f7cad5724a8b6e85567a
dev2820 <second28200@gmail.com>
2021-11-16 18:34:47 +0900

    범용 Button(Btn) 생성

---
+++ frontend/src/components/Btn.vue
@@ -0,0 +1,34 @@
+<template>
+    <button @click="$emit('click',$event)" 
+        :class="{
+            'white':'white'===theme,
+            'primary':'primary'===theme
+        }"
+    >
+        <slot/>
+    </button>
+</template>
+
+<script>
+export default {
+    props:{
+        theme: {
+            type: String,
+            required: false,
+            default:'primary',
+            validator: (value)=> {
+                return ['white','primary',''].indexOf(value) !== -1;
+            }
+        }
+    }
+}
+</script>
+
+<style>
+.white {
+    background-color: var(--background-color);
+}
+.primary {
+    background-color: var(--primary-color);
+}
+</style>
