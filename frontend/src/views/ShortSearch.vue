dce6ec7caf25b264bf125e829d7b4d667d6059c1
dev2820 <second28200@gmail.com>
2021-11-16 15:18:23 +0900

    shorts 검색 화면에 shortSummary 컴포넌트 추가

--- frontend/src/views/ShortSearch.vue
+++ frontend/src/views/ShortSearch.vue
@@ -1,12 +1,26 @@
 <template>
     <div>
         shorts search
+        <ShortSummary v-for="(value,index) in shortList" :key="index"/>
     </div>
 </template>
 
 <script>
+import ShortSummary from '../components/ShortSummary.vue'
+import {mapState} from 'vuex'
 export default {
-
+    name: 'ShortSearch',
+    components: { ShortSummary },
+    data() {
+        return {
+            shortList
+        }
+    },
+    computed: {
+        ...mapState([
+            'shortsList'
+        ])
+    }
 }
 </script>
 
