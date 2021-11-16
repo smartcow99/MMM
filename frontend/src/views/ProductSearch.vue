5a7bc33e912083fc648f8e2b5e05b4aa5d5727d2
dev2820 <second28200@gmail.com>
2021-11-16 20:10:54 +0900

    requestSearch 없어서 채움(productSearch)

--- frontend/src/views/ProductSearch.vue
+++ frontend/src/views/ProductSearch.vue
@@ -34,7 +34,8 @@ export default {
     },
     methods: {
         ...mapActions([
-            'requestProductList'
+            'requestProductList',
+            'requestSearch',
         ])
     }
 }
