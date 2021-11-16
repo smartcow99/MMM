3b3a4c97d77441d8722c68acd4da313d1b9ad9ff
dev2820 <second28200@gmail.com>
2021-11-16 14:43:39 +0900

    aside에 사용할 profile 컴포넌트 추가

--- frontend/src/components/Profile.vue
+++ frontend/src/components/Profile.vue
@@ -1,10 +1,16 @@
 <template>
     <div class="profile">
-        <img :src="profileImage"/>
-        <h2>{{nickname}}</h2>
-        <router-link link='/mypage'>마이페이지</router-link>
-        <router-link link='/channel'>내 채널</router-link>
-        <button>업로드</button>
+        <img v-if="userInfo['isLogined']" :src="profileImage"/>
+        <img v-else src="@/assets/images/defaultProfile.png"/>
+        <div v-if="userInfo['isLogined']" class="user-info">
+            <h2>{{nickname}}</h2>
+            <router-link link='/mypage'>마이페이지</router-link>
+            <router-link link='/channel'>내 채널</router-link>
+            <button>업로드</button>
+        </div>
+        <div v-else>
+            <button @click="$emit('openLogin',$event)">로그인</button>
+        </div>
     </div>
 </template>
 
@@ -12,8 +18,7 @@
 export default {
     name: 'Profile',
     props: {
-        profileImage: String,
-        nickname: String
+        'userInfo': Object,// isLogined: boolean, 
     }
 }
 </script>
