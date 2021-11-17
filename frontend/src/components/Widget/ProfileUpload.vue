02bbb65b4162e35e737ed4326558089523e7e5b8
dev2820 <second28200@gmail.com>
2021-11-17 22:59:15 +0900

    프로필 업로드 컴포넌트 생성 및 연결

---
+++ frontend/src/components/Widget/ProfileUpload.vue
@@ -0,0 +1,13 @@
+<template>
+    프로필업로드
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- frontend/src/views/UserInfo.vue
+++ frontend/src/views/UserInfo.vue
@@ -2,7 +2,7 @@
     <div>
         <div class="box">
             <img :src="userInfo['profile']" alt="내 프로필"/>
-            <Btn theme="primary">프로필 사진 변경</Btn>
+            <Btn theme="primary" @click="openUpload">프로필 사진 변경</Btn>
             <p>사진 용량 제한</p>
         </div>
         <div class="box">
@@ -18,20 +18,28 @@
                 <span>비밀번호 재입력</span><input type="password"/>
             </div>
         </div>
+        <BlurCard v-if="uploadClicked" @close="closeUpload">
+            <ProfileUpload/>
+        </BlurCard>
     </div>
 </template>
 
 <script>
 import Btn from '@/components/Btn.vue'
 import { mapState } from 'vuex'
+import BlurCard from '../components/BlurCard.vue'
+import ProfileUpload from '../components/widget/ProfileUpload.vue'
 export default {
     name: 'UserInfo',
     components: {
-        Btn
+        Btn,
+        BlurCard,
+        ProfileUpload
     },
     data() {
         return {
             name:'',
+            uploadClicked:false,
             changePasswordState:false
         }
     },
@@ -44,6 +52,12 @@ export default {
         changePassword() {
             //비번 변경 넣긴 했는데 아마 못쓰게 막아놨습니다.
             alert('! 데모버전에서 비밀번호 변경은 막혀있습니다. !')
+        },
+        openUpload() {
+            this.uploadClicked = true;
+        },
+        closeUpload() {
+            this.uploadClicked = false;
         }
     }
 }
