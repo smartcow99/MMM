35786ad02ed994b62af6d776233b399da73a745b
Tuna-is-DW <ydw5210@gmail.com>
2021-11-18 00:47:54 +0900

    merge

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,39 +3,6 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
-<<<<<<< Updated upstream
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { 
-    faUserSecret,
-    faSearch,
-    faTimes,
-    faUpload,
-    faAngleLeft,
-    faAngleRight,
-    faEye,
-    faComment,
-    faHeart,
-    faStar,
-    faStarHalfAlt
-} from '@fortawesome/free-solid-svg-icons'
-import {
-    faStar as farStar
-} from '@fortawesome/free-regular-svg-icons'
-
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-library.add(faUserSecret)
-library.add(faSearch)
-library.add(faTimes)
-library.add(faUpload)
-library.add(faAngleLeft)
-library.add(faAngleRight)
-library.add(faEye)
-library.add(faComment)
-library.add(faHeart)
-library.add(faStar)
-library.add(farStar)
-library.add(faStarHalfAlt)
-=======
 import { library } from "@fortawesome/fontawesome-svg-core";
 import {
   faUserSecret,
@@ -58,8 +25,6 @@ library.add(faAngleRight);
 library.add(faEye);
 library.add(faComment);
 library.add(faHeart);
-library.add(faUpload);
->>>>>>> Stashed changes
 
 const app = createApp(App);
 app.component("font-awesome-icon", FontAwesomeIcon);
