69ee5f84d301fe3a2b3c3dd0a4cac26cec5b8123
spaff13 <spaff316@gmail.com>
2021-11-22 23:31:26 +0900

    cor

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const session = require('express-session');
-
+const cors = require('cors');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -25,6 +25,7 @@ app.use(session({
     Secure: false
   }
 }));
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
