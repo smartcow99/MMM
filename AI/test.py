6b7ed080e15e8d91a27e5c00b64e93b9e60d7920
Jiwonii97 <wldnjs3633@naver.com>
2021-11-24 22:03:44 +0900

    change test.py

--- AI/test.py
+++ AI/test.py
@@ -3,4 +3,5 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 img = cv2.imread('testimg.jpg')                                               # image.png 로드
-img = cv2.resize(img, None, fx=1/2, fy=1/2, interpolation=cv2.INTER_AREA)   # image.png를 절반으로 줄이기
+# img = cv2.resize(img, None, fx=1/2, fy=1/2, interpolation=cv2.INTER_AREA)   # image.png를 절반으로 줄이기
+cv2. imwrite('test.jpg',img)
