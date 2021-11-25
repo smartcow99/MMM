bccdff592e541f8ab0ef459e2ba7cdda90d84bfe
spaff13 <spaff316@gmail.com>
2021-11-25 21:31:35 +0900

    로그인 임시 통과

--- backend/my_modules/logincheck.js
+++ backend/my_modules/logincheck.js
@@ -2,5 +2,6 @@ module.exports = (req, res, next) =>{
   if(req.session.islogined&&req.session.cid != undefined)
     next();
   else 
-    res.status(401).send('not logined');
+    {req.session.cid = 1; next()}
+    // res.status(401).send('not logined');
 }
